09dce04060e2b09d15f4ff72bd42cb39d9f99911
zero <mcheung@bisu.io>
2020-02-10 20:04:55 -0600

    fixed an issue with being unable to retrieve the api

--- src/main/java/com/mcheung/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ src/main/java/com/mcheung/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -2,6 +2,7 @@ package com.mcheung.rest.webservices.restfulwebservices.todo;
 
 import com.mcheung.rest.webservices.restfulwebservices.todo.Todo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class TodoResource {
 
     @Autowired
     private TodoHardcodedService todoService;
 
-    @GetMapping("/users/{username}/todos")
+    @GetMapping(path="/users/{username}/todos")
     public List<Todo> getAllTodos(@PathVariable String username) {
         return todoService.findAll();
     }
